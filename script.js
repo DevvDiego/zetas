0c59d1f36961a070de0fbe67915b668ca38dd32b
diego-ans <83733071+diego-ans@users.noreply.github.com>
2023-05-09 18:45:54 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const music = new Audio('assets/sounds/aud_2.wav');
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    document.getElementById("start").addEventListener("click", ()=>{
-        
-        const startScreen= document.getElementById("start");
-        const content = document.getElementById("content");
+    const scr1 = document.getElementById("screen1");
+    const scr2 = document.getElementById("screen2");
+    const timer = document.getElementById("timer");
 
-        startScreen.style.opacity="0";
-        startScreen.style.display="none";
 
-        content.style.display="flex";
+    scr1.addEventListener("click",e=>{
+        scr1.style.display="none";
+        scr2.style.display="block";
 
         startPlaying();
+        startTimer(timer);
+
     })
+
 
 })
 
@@ -24,12 +26,23 @@ function startPlaying(){
     music.playbackRate = 1;
     
 
-    document.getElementById("stop").addEventListener("click", ()=>{
-        music.pause();
-
-    })
-
-    document.getElementById("init").addEventListener("click", ()=>{
-        music.play();
-    })
 }
+
+function startTimer(display) {
+    let timer = 0, hours, minutes, seconds;
+    setInterval(function () {
+      hours = parseInt(timer / 3600, 10);
+      minutes = parseInt((timer % 3600) / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+  
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      display.textContent = hours + ":" + minutes + ":" + seconds;
+  
+      timer++;
+    }, 1000);
+}
+  
+  
